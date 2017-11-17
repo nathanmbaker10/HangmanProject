d697af2affd0c8fc4b4aaa8eafa72c582944cbab
Nathan Baker <nathanmbaker@gmail.com>
2017-11-17 11:45:26 -0800

    Almost Done

---
+++ reloadSelectBox.js
@@ -0,0 +1,28 @@
+function reloadTextBox() {
+    document.getElementById("selectBox").innerHTML = " <option value=\"a\">a</option>\n" +
+        "        <option value=\"b\">b</option>\n" +
+        "        <option value=\"c\">c</option>\n" +
+        "        <option value=\"d\">d</option>\n" +
+        "        <option value=\"e\">e</option>\n" +
+        "        <option value=\"f\">f</option>\n" +
+        "        <option value=\"g\">g</option>\n" +
+        "        <option value=\"h\">h</option>\n" +
+        "        <option value=\"i\">i</option>\n" +
+        "        <option value=\"j\">j</option>\n" +
+        "        <option value=\"k\">k</option>\n" +
+        "        <option value=\"l\">l</option>\n" +
+        "        <option value=\"m\">m</option>\n" +
+        "        <option value=\"n\">n</option>\n" +
+        "        <option value=\"o\">o</option>\n" +
+        "        <option value=\"p\">p</option>\n" +
+        "        <option value=\"q\">q</option>\n" +
+        "        <option value=\"r\">r</option>\n" +
+        "        <option value=\"s\">s</option>\n" +
+        "        <option value=\"t\">t</option>\n" +
+        "        <option value=\"u\">u</option>\n" +
+        "        <option value=\"v\">v</option>\n" +
+        "        <option value=\"w\">w</option>\n" +
+        "        <option value=\"x\">x</option>\n" +
+        "        <option value=\"y\">y</option>\n" +
+        "        <option value=\"z\">z</option>";
+}
